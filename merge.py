41cd13a202dc48a30ea9369e0d2bd8a3e26db949
chandni kumari <ichandnikumari15@gmail.com>
2024-07-13 16:06:04 +0530

    files

--- merge.py
+++
@@ -1,6 +0,0 @@
-for i in range(0, len(string), k):
-        substr = []
-        for j in range(i, i+k):
-            if string[j] not in substr:
-                substr.append(string[j])
-        print(''.join(substr))
